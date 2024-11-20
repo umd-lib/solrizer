e37189f4df7c7a409770588329e058d7ac251dd4
Peter Eichman <peichman@umd.edu>
2024-11-20 11:55:20 -0500

    LIBFCREPO-1591. Don't depend on local time zone in `solr_datetime()`
    - If the provided `dt_string` does not have a time zone specified, always use UTC.
    - Removed the "freezegun" dependency from tests
    
    https://umd-dit.atlassian.net/browse/LIBFCREPO-1591

--- src/solrizer/indexers/utils.py
+++ src/solrizer/indexers/utils.py
@@ -11,7 +11,13 @@ def solr_datetime(dt_string: str) -> str:
     as `YYYY` or `YYYY-MM`) will result in a `ValueError`.
 
     If no time is specified, this function depends on the `datetime`
-    library's behavior of defaulting the time to midnight (00:00:00)
-    in local time."""
-    dt = datetime.fromisoformat(dt_string).astimezone(timezone.utc)
+    library's behavior of defaulting the time to midnight (00:00:00).
+    Partial times are padded out with zeroes.
+
+    If there is no timezone information, the timezone is set to UTC."""
+    dt = datetime.fromisoformat(dt_string)
+    if dt.tzinfo is None:
+        dt = dt.replace(tzinfo=timezone.utc)
+    else:
+        dt = dt.astimezone(timezone.utc)
     return dt.isoformat(sep='T').replace('+00:00', 'Z')
--- tests/indexers/test_utils.py
+++ tests/indexers/test_utils.py
@@ -1,5 +1,4 @@
 import pytest
-from freezegun import freeze_time
 
 from solrizer.indexers.utils import solr_datetime
 
@@ -21,14 +20,13 @@ def test_solr_datetime_invalid_strings(dt_string):
 @pytest.mark.parametrize(
     ('dt_string', 'expected_value'),
     [
-        ('2024-11-19', '2024-11-19T05:00:00Z'),
-        ('2024-11-19T09', '2024-11-19T14:00:00Z'),
-        ('2024-11-19T09:17', '2024-11-19T14:17:00Z'),
-        ('2024-11-19T09:17:32', '2024-11-19T14:17:32Z'),
+        ('2024-11-19', '2024-11-19T00:00:00Z'),
+        ('2024-11-19T09', '2024-11-19T09:00:00Z'),
+        ('2024-11-19T09:17', '2024-11-19T09:17:00Z'),
+        ('2024-11-19T09:17:32', '2024-11-19T09:17:32Z'),
         ('2024-11-19T09:17:32-08:00', '2024-11-19T17:17:32Z'),
         ('2024-11-19T09:17:32+00:00', '2024-11-19T09:17:32Z'),
     ]
 )
-@freeze_time(tz_offset=-5)
 def test_solr_datetime(dt_string, expected_value):
     assert solr_datetime(dt_string) == expected_value
