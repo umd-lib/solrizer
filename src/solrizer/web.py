20eae6e2b5586415d0782c3f07a50ab3a75f727d
Peter Eichman <peichman@umd.edu>
2025-01-16 14:52:45 -0500

    LIBFCREPO-1602. Allow reading configuration from files.
    - read config from file in env var ends in `_FILE`
    - supports loading both yaml and json config files
    
    https://umd-dit.atlassian.net/browse/LIBFCREPO-1602

--- src/solrizer/web.py
+++ src/solrizer/web.py
@@ -1,7 +1,10 @@
 import json
 import logging
+from pathlib import Path
 from time import strftime
+from typing import MutableMapping
 
+import yaml
 from flask import Flask, request
 from plastron.client import Client, Endpoint
 from plastron.models import ModelClassError, guess_model
@@ -24,9 +27,46 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
+LOADERS = {
+    '.json': json.load,
+    '.yml': yaml.safe_load,
+    '.yaml': yaml.safe_load,
+}
+
+
+def load_config_from_files(config: MutableMapping):
+    """Iterates over the keys in `config`. For any with the format "{NAME}_FILE",
+    treat its value as a filename. Reads that file using the appropriate loader
+    ("*.json" files use `json.load`, and "*.yml" and "*.yaml" files use `yaml.safe_load`)
+    and set the config key "{NAME}" to the return value of the loader.
+
+    Ignores a "{NAME}_FILE" key if "{NAME}" is already defined in config (i.e.,
+    "{NAME}" takes precedence over "{NAME}_FILE").
+
+    Raises a `RuntimeError` if the file suffix is unrecognized, or if the file
+    cannot be opened."""
+    file_keys = [k for k in config.keys() if k.endswith('_FILE')]
+    for file_key in file_keys:
+        # strip the "_FILE" suffix
+        key = file_key[:-5]
+        if key not in config:
+            # only load from file if there isn't already a config value with this key
+            file = Path(config[file_key])
+            try:
+                loader = LOADERS[file.suffix]
+            except KeyError as e:
+                raise RuntimeError(f'Cannot open a config file with suffix "{file.suffix}"') from e
+            try:
+                with file.open() as fh:
+                    config[key] = loader(fh)
+            except FileNotFoundError as e:
+                raise RuntimeError(f'Config file "{file}" not found') from e
+
+
 def create_app():
     app = Flask(__name__)
     app.config.from_prefixed_env('SOLRIZER')
+    load_config_from_files(app.config)
 
     client = Client(
         endpoint=Endpoint(app.config['FCREPO_ENDPOINT']),
