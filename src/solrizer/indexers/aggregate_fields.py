0f1239a03b9ab2d01d44901c730de121233eb048
Peter Eichman <peichman@umd.edu>
2025-02-27 21:23:09 -0500

    LIBFCREPO-1602. Added aggregate_fields indexer.
    - uses `jq` to retrieve values from the current state of the index document
    - writes multi-valued field(s) using those values
    
    https://umd-dit.atlassian.net/browse/LIBFCREPO-1602

---
+++ src/solrizer/indexers/aggregate_fields.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+
+import jq
+
+from solrizer.indexers import IndexerContext, SolrFields, IndexerError
+
+
+def aggregate_fields(ctx: IndexerContext) -> SolrFields:
+    fields = defaultdict(list)
+    try:
+        jq_filters = {k: [jq.compile(q) for q in v] for k, v in ctx.settings.items()}
+    except ValueError as e:
+        raise IndexerError(f'Unable to compile aggregate field query: {e}')
+
+    for field, queries in jq_filters.items():
+        for query in queries:
+            fields[field].extend(v for v in query.input_value(ctx.doc).all() if v is not None)
+
+    return fields
