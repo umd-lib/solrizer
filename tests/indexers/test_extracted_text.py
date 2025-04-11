8a753fb0a717c0439c491754b3bb80027d5b17c6
Peter Eichman <peichman@umd.edu>
2025-04-11 17:34:06 -0400

    LIBFCREPO-1626. Add test of hOCR extracted text indexer.
    https://umd-dit.atlassian.net/browse/LIBFCREPO-1626

--- tests/indexers/test_extracted_text.py
+++ tests/indexers/test_extracted_text.py
@@ -74,6 +74,49 @@ def test_get_page_text_alto(datadir, monkeypatch):
     assert page_text.tagged
 
 
+def test_get_page_text_hocr(datadir, monkeypatch):
+    mock_resource = MagicMock(spec=PCDMObjectResource)
+
+    def mock_get_file(rdf_type=None, **_kwargs):
+        if rdf_type == pcdmuse.ExtractedText:
+            return datadir / 'sample.hocr'
+        elif rdf_type == pcdmuse.PreservationMasterFile:
+            return MockBinaryResource(datadir / 'litmss-077848-0002.tif')
+        else:
+            return None
+
+    mock_resource.get_file.side_effect = mock_get_file
+    expected_text = (
+        ' |n=0&xywh=0,2924,282,1320 '
+        ' |n=0&xywh=0,0,404,404 '
+        'Page|n=0&xywh=340,473,230,85 '
+        '1|n=0&xywh=675,472,77,84 '
+        'Additions|n=0&xywh=1238,436,526,159 '
+        ':|n=0&xywh=1773,485,71,100 '
+        'nd|n=0&xywh=1850,406,154,127 '
+        'g¢orrections|n=0&xywh=2055,395,656,161 '
+        'for|n=0&xywh=2788,433,164,118 '
+        'the|n=0&xywh=3028,425,164,96 '
+        'manuseript|n=0&xywh=3312,420,596,108 '
+        'of|n=0&xywh=3980,412,112,94 '
+        '"The|n=0&xywh=1725,663,219,75 '
+        'Future|n=0&xywh=2007,658,355,100 '
+        'Is|n=0&xywh=2435,654,111,80 '
+        'Now!|n=0&xywh=2607,654,224,74 '
+        'A|n=0&xywh=3690,692,26,25 '
+        'By|n=0&xywh=2235,762,129,86 '
+        'Ka|n=0&xywh=2429,757,101,74 '
+        'atherine|n=0&xywh=2497,696,446,192 '
+        'Anne|n=0&xywh=3029,761,226,63 '
+        'forter|n=0&xywh=3324,738,346,86 '
+        ' |n=0&xywh=3443,909,959,151'
+    )
+    page_text = get_text_page(mock_resource, 0)
+    assert page_text.text == expected_text
+    assert page_text.page_index == 0
+    assert page_text.tagged
+
+
 def test_unsupported_xml_ocr_format(datadir):
     mock_resource = MagicMock(spec=PCDMObjectResource)
 
