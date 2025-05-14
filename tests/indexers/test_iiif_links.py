8ddc667e2870ebfc4a103b11b24dd20ed402ef9d
Peter Eichman <peichman@umd.edu>
2025-05-14 16:16:32 -0400

    LIBFCREPO-1640. Handle KeyError in iiif_links page__has_file lookup.
    If there is no `page__has_file` item, or it is an empty list, uses "static:unavailable" for the thumbnail identifier.
    
    https://umd-dit.atlassian.net/browse/LIBFCREPO-1640

--- tests/indexers/test_iiif_links.py
+++ tests/indexers/test_iiif_links.py
@@ -2,11 +2,42 @@ import pytest
 from plastron.models.ldp import LDPContainer
 from plastron.models.ore import Proxy
 from plastron.models.umd import Item
-from plastron.repo.pcdm import ProxyIterator
 from rdflib import URIRef
 
 from solrizer.indexers import IndexerContext
 from solrizer.indexers.iiif_links import iiif_identifier, iiif_links_fields
+
+
+@pytest.fixture
+def mock_repo(create_mock_repo):
+    return create_mock_repo({
+        '/proxy1': Proxy(proxy_for=URIRef('/url1'), next=URIRef('/proxy2')),
+        '/proxy2': Proxy(proxy_for=URIRef('/url2'), next=URIRef('/proxy3')),
+        '/proxy3': Proxy(proxy_for=URIRef('/url3')),
+        '/foo': Item(first=URIRef('/proxy1')),
+        '/foo/f': LDPContainer(),
+        '/foo/m': LDPContainer(),
+    })
+
+
+@pytest.fixture
+def create_ctx(mock_repo):
+    def _ctx(members):
+        return IndexerContext(
+            repo=mock_repo,
+            resource=mock_repo['/foo'],
+            model_class=Item,
+            doc={
+                'id': 'http://example.com/fcrepo/foo',
+                'object__has_member': members,
+            },
+            config={
+                'IIIF_IDENTIFIER_PREFIX': 'fcrepo:',
+                'IIIF_THUMBNAIL_URL_PATTERN': 'http://iiif.example.com/thumbnail/{+id}',
+                'IIIF_MANIFESTS_URL_PATTERN': 'http://iiif.example.com/manifest/{+id}',
+            },
+        )
+    return _ctx
 
 
 @pytest.mark.parametrize(
@@ -21,7 +52,7 @@ def test_iiif_identifier(path, prefix, expected_identifier):
     assert iiif_identifier(path, prefix) == expected_identifier
 
 
-def test_iiif_links_fields(monkeypatch, proxies, create_mock_repo):
+def test_iiif_links_fields(proxies, create_ctx):
     members = [
         {'id': '/url1', 'page__title__txt': 'Bar 1', 'page__has_file': [
             {'id': 'http://example.com/fcrepo/foo/obj1/fileX'},
@@ -33,29 +64,7 @@ def test_iiif_links_fields(monkeypatch, proxies, create_mock_repo):
             {'id': 'http://example.com/fcrepo/foo/obj3/fileX'},
         ]},
     ]
-    mock_repo = create_mock_repo({
-        '/proxy1': Proxy(proxy_for=URIRef('/url1'), next=URIRef('/proxy2')),
-        '/proxy2': Proxy(proxy_for=URIRef('/url2'), next=URIRef('/proxy3')),
-        '/proxy3': Proxy(proxy_for=URIRef('/url3')),
-        '/foo': Item(first=URIRef('/proxy1')),
-        '/foo/f': LDPContainer(),
-        '/foo/m': LDPContainer(),
-    })
-    ctx = IndexerContext(
-        repo=mock_repo,
-        resource=mock_repo['/foo'],
-        model_class=Item,
-        doc={
-            'id': 'http://example.com/fcrepo/foo',
-            'object__has_member': members,
-        },
-        config={
-            'IIIF_IDENTIFIER_PREFIX': 'fcrepo:',
-            'IIIF_THUMBNAIL_URL_PATTERN': 'http://iiif.example.com/thumbnail/{+id}',
-            'IIIF_MANIFESTS_URL_PATTERN': 'http://iiif.example.com/manifest/{+id}',
-        },
-    )
-    fields = iiif_links_fields(ctx)
+    fields = iiif_links_fields(create_ctx(members))
     assert fields == {
         'iiif_manifest__id': 'fcrepo:foo',
         'iiif_manifest__uri': 'http://iiif.example.com/manifest/fcrepo:foo',
@@ -66,6 +75,33 @@ def test_iiif_links_fields(monkeypatch, proxies, create_mock_repo):
         ],
         'iiif_thumbnail_sequence__uris': [
             'http://iiif.example.com/thumbnail/fcrepo:foo:obj1:fileX',
+            'http://iiif.example.com/thumbnail/fcrepo:foo:obj2:fileX',
+            'http://iiif.example.com/thumbnail/fcrepo:foo:obj3:fileX',
+        ]
+    }
+
+
+def test_iiif_links_no_file_on_page(proxies, create_ctx):
+    members = [
+        {'id': '/url1', 'page__title__txt': 'Bar 1'},
+        {'id': '/url2', 'page__title__txt': 'Bar 2', 'page__has_file': [
+            {'id': 'http://example.com/fcrepo/foo/obj2/fileX'},
+        ]},
+        {'id': '/url3', 'page__title__txt': 'Bar 3', 'page__has_file': [
+            {'id': 'http://example.com/fcrepo/foo/obj3/fileX'},
+        ]},
+    ]
+    fields = iiif_links_fields(create_ctx(members))
+    assert fields == {
+        'iiif_manifest__id': 'fcrepo:foo',
+        'iiif_manifest__uri': 'http://iiif.example.com/manifest/fcrepo:foo',
+        'iiif_thumbnail_sequence__ids': [
+            'static:unavailable',
+            'fcrepo:foo:obj2:fileX',
+            'fcrepo:foo:obj3:fileX',
+        ],
+        'iiif_thumbnail_sequence__uris': [
+            'http://iiif.example.com/thumbnail/static:unavailable',
             'http://iiif.example.com/thumbnail/fcrepo:foo:obj2:fileX',
             'http://iiif.example.com/thumbnail/fcrepo:foo:obj3:fileX',
         ]
