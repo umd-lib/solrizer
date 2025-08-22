52794f81efe71f38c5598478c0238a1ae955e3de
Peter Eichman <peichman@umd.edu>
2025-08-22 16:40:17 -0400

    LIBFCREPO-1668. Ensure that the file chosen for the thumbnail is an image.
    https://umd-dit.atlassian.net/browse/LIBFCREPO-1668

--- src/solrizer/indexers/iiif_links.py
+++ src/solrizer/indexers/iiif_links.py
@@ -68,18 +68,86 @@ def iiif_links_fields(ctx: IndexerContext) -> SolrFields:
     }
 
 
-def get_first_file_identifier(ctx: IndexerContext, page: SolrFields) -> str:
-    """Given an `IndexerContext` and a page dictionary, returns the IIIF
-    identifier for the first file of that page, or "static:unavailable" if
-    the URI of the first file cannot be retrieved from the `page` for any
-    reason."""
-    try:
-        file: dict[str, Any] = page['page__has_file'][0]
-        file_uri = file['id']
-    except KeyError:
-        return 'static:unavailable'
+def is_preservation_file(file: SolrFields) -> bool:
+    """Checks for the presence of `pcdmuse:PreservationMasterFile` in the file
+    document's `file__rdf_type__curies` field."""
+
+    return 'pcdmuse:PreservationMasterFile' in file.get('file__rdf_type__curies', [])
+
+
+def is_image_file(file: SolrFields, mime_subtype: str = None) -> bool:
+    """Checks the file document's `file__mime_type__str` field to see if it has
+    the "image" type. If the `mime_subtype` is given, the file's MIME type must
+    exactly match "image/{mime_subtype}".
+
+    ```pycon
+    >>> jpeg_file = {'file__mime_type__str': 'image/jpeg'}
+    >>> tiff_file = {'file__mime_type__str': 'image/tiff'}
+
+    >>> is_image_file(jpeg_file)
+    True
+
+    >>> is_image_file(tiff_file)
+    True
+
+    # subtype must match exactly
+    >>> is_image_file(tiff_file, 'jpeg')
+    False
+
+    ```
+    """
+
+    mime_type = file.get('file__mime_type__str', '')
+    if mime_subtype is None:
+        return mime_type.startswith('image/')
     else:
-        return iiif_identifier(
-            repo_path=ctx.repo.endpoint.repo_path(file_uri),
-            prefix=ctx.config['IIIF_IDENTIFIER_PREFIX'],
-        )
+        return mime_type == f'image/{mime_subtype}'
+
+
+def get_best_image_file(page: SolrFields) -> SolrFields:
+    """Get the "best" image file document, according to the following order of preference:
+
+    1. First file with RDF type `pcdmuse:PreservationMasterFile` and MIME type matching `image/*`
+    2. First file with MIME type `image/tiff`
+    3. First file with MIME type `image/jpeg`
+    4. First file with MIME type matching `image/*`
+
+    If none of these criteria are satisfied, raises an `ImageUnavailable` exception.
+    """
+    try:
+        files: list[SolrFields] = page['page__has_file']
+    except KeyError:
+        raise ImageUnavailable
+
+    if preservation_files := [f for f in files if (is_preservation_file(f) and is_image_file(f))]:
+        return preservation_files[0]
+    if tiff_files := [f for f in files if is_image_file(f, 'tiff')]:
+        return tiff_files[0]
+    if jpeg_files := [f for f in files if is_image_file(f, 'jpeg')]:
+        return jpeg_files[0]
+    if image_files := [f for f in files if is_image_file(f)]:
+        return image_files[0]
+
+    raise ImageUnavailable
+
+
+def get_first_file_identifier(ctx: IndexerContext, page: SolrFields) -> str:
+    """Given an `IndexerContext` and a page dictionary, returns the IIIF identifier
+    for the best file to use as a thumbnail for that page. Returns "static:unavailable"
+    if page has no suitable files. See `get_best_image_file()` for the criteria for
+    "best" file."""
+
+    try:
+        file = get_best_image_file(page)
+    except ImageUnavailable:
+        return 'static:unavailable'
+
+    file_uri = file['id']
+    return iiif_identifier(
+        repo_path=ctx.repo.endpoint.repo_path(file_uri),
+        prefix=ctx.config['IIIF_IDENTIFIER_PREFIX'],
+    )
+
+
+class ImageUnavailable(Exception):
+    pass
--- tests/indexers/test_iiif_links.py
+++ tests/indexers/test_iiif_links.py
@@ -1,11 +1,133 @@
+from unittest.mock import MagicMock
+
 import pytest
+from plastron.client import Client, Endpoint
 from plastron.models.ldp import LDPContainer
 from plastron.models.ore import Proxy
 from plastron.models.umd import Item
+from plastron.repo import Repository
 from rdflib import URIRef
 
 from solrizer.indexers import IndexerContext
-from solrizer.indexers.iiif_links import iiif_identifier, iiif_links_fields
+from solrizer.indexers.iiif_links import iiif_identifier, iiif_links_fields, get_first_file_identifier
+
+
+@pytest.mark.parametrize(
+    ('path', 'prefix', 'expected_identifier'),
+    [
+        ('/foo/bar', 'fcrepo:', 'fcrepo:foo:bar'),
+        ('/foo', 'fcrepo:', 'fcrepo:foo'),
+        ('/foo', '', 'foo'),
+    ]
+)
+def test_iiif_identifier(path, prefix, expected_identifier):
+    assert iiif_identifier(path, prefix) == expected_identifier
+
+
+@pytest.mark.parametrize(
+    ('files', 'expected_id'),
+    [
+        # preservation master first
+        (
+            [
+                ('/master', ['pcdm:File', 'pcdmuse:PreservationMasterFile'], 'image/tiff'),
+                ('/pdf', ['pcdm:File'], 'application/pdf'),
+            ],
+            'fcrepo:master',
+        ),
+        # preservation master second
+        (
+            [
+                ('/pdf', ['pcdm:File'], 'application/pdf'),
+                ('/master', ['pcdm:File', 'pcdmuse:PreservationMasterFile'], 'image/tiff'),
+            ],
+            'fcrepo:master',
+        ),
+        # preservation master after TIFF
+        (
+            [
+                ('/tiff', ['pcdm:File'], 'image/tiff'),
+                ('/master', ['pcdm:File', 'pcdmuse:PreservationMasterFile'], 'image/tiff'),
+            ],
+            'fcrepo:master',
+        ),
+        # TIFF before JPEG, no preservation master
+        (
+            [
+                ('/tiff', ['pcdm:File'], 'image/tiff'),
+                ('/jpeg', ['pcdm:File'], 'image/jpeg'),
+            ],
+            'fcrepo:tiff',
+        ),
+        # TIFF after JPEG, no preservation master
+        (
+            [
+                ('/jpeg', ['pcdm:File'], 'image/jpeg'),
+                ('/tiff', ['pcdm:File'], 'image/tiff'),
+            ],
+            'fcrepo:tiff',
+        ),
+        # only TIFF, no preservation master
+        (
+            [
+                ('/tiff', ['pcdm:File'], 'image/tiff'),
+            ],
+            'fcrepo:tiff',
+        ),
+        # only JPEG, no preservation master
+        (
+            [
+                ('/jpeg', ['pcdm:File'], 'image/jpeg'),
+            ],
+            'fcrepo:jpeg',
+        ),
+        # other image only
+        (
+            [
+                ('/png', ['pcdm:File'], 'image/png'),
+            ],
+            'fcrepo:png',
+        ),
+        # preservation master not a TIFF file
+        (
+            [
+                ('/pdf', ['pcdm:File', 'pcdmuse:PreservationMasterFile'], 'application/pdf'),
+            ],
+            'static:unavailable',
+        ),
+        # no suitable image file
+        (
+            [
+                ('/pdf', ['pcdm:File'], 'application/pdf'),
+                ('/xml', ['pcdm:File', 'pcdmuse:ExtractedText'], 'application/xml'),
+            ],
+            'static:unavailable',
+        ),
+        # no files
+        (
+            [],
+            'static:unavailable',
+        ),
+    ]
+)
+def test_get_file_identifier(files, expected_id):
+    page = {
+        'page__has_file': [
+            {
+                'id': f'http://example.com/fcrepo/rest{repo_path}',
+                'file__rdf_type__curies': rdf_types,
+                'file__mime_type__str': mime_type,
+            }
+            for repo_path, rdf_types, mime_type in files
+        ]
+    }
+    mock_context = MagicMock(
+        spec=IndexerContext,
+        config={'IIIF_IDENTIFIER_PREFIX': 'fcrepo:'},
+        repo=Repository(client=Client(endpoint=Endpoint('http://example.com/fcrepo/rest'))),
+    )
+    iiif_id = get_first_file_identifier(mock_context, page)
+    assert iiif_id == expected_id
 
 
 @pytest.fixture
@@ -37,31 +159,32 @@ def create_ctx(mock_repo):
                 'IIIF_MANIFESTS_URL_PATTERN': 'http://iiif.example.com/manifest/{+id}',
             },
         )
+
     return _ctx
-
-
-@pytest.mark.parametrize(
-    ('path', 'prefix', 'expected_identifier'),
-    [
-        ('/foo/bar', 'fcrepo:', 'fcrepo:foo:bar'),
-        ('/foo', 'fcrepo:', 'fcrepo:foo'),
-        ('/foo', '', 'foo'),
-    ]
-)
-def test_iiif_identifier(path, prefix, expected_identifier):
-    assert iiif_identifier(path, prefix) == expected_identifier
 
 
 def test_iiif_links_fields(proxies, create_ctx):
     members = [
         {'id': '/url1', 'page__title__txt': 'Bar 1', 'page__has_file': [
-            {'id': 'http://example.com/fcrepo/foo/obj1/fileX'},
+            {
+                'id': 'http://example.com/fcrepo/foo/obj1/fileX',
+                'file__rdf_type__curies': ['pcdmuse:PreservationMasterFile'],
+                'file__mime_type__str': 'image/tiff',
+            },
         ]},
         {'id': '/url2', 'page__title__txt': 'Bar 2', 'page__has_file': [
-            {'id': 'http://example.com/fcrepo/foo/obj2/fileX'},
+            {
+                'id': 'http://example.com/fcrepo/foo/obj2/fileX',
+                'file__rdf_type__curies': ['pcdmuse:PreservationMasterFile'],
+                'file__mime_type__str': 'image/tiff',
+            },
         ]},
         {'id': '/url3', 'page__title__txt': 'Bar 3', 'page__has_file': [
-            {'id': 'http://example.com/fcrepo/foo/obj3/fileX'},
+            {
+                'id': 'http://example.com/fcrepo/foo/obj3/fileX',
+                'file__rdf_type__curies': ['pcdmuse:PreservationMasterFile'],
+                'file__mime_type__str': 'image/tiff',
+            },
         ]},
     ]
     fields = iiif_links_fields(create_ctx(members))
@@ -85,10 +208,18 @@ def test_iiif_links_no_file_on_page(proxies, create_ctx):
     members = [
         {'id': '/url1', 'page__title__txt': 'Bar 1'},
         {'id': '/url2', 'page__title__txt': 'Bar 2', 'page__has_file': [
-            {'id': 'http://example.com/fcrepo/foo/obj2/fileX'},
+            {
+                'id': 'http://example.com/fcrepo/foo/obj2/fileX',
+                'file__rdf_type__curies': ['pcdmuse:PreservationMasterFile'],
+                'file__mime_type__str': 'image/tiff',
+            },
         ]},
         {'id': '/url3', 'page__title__txt': 'Bar 3', 'page__has_file': [
-            {'id': 'http://example.com/fcrepo/foo/obj3/fileX'},
+            {
+                'id': 'http://example.com/fcrepo/foo/obj3/fileX',
+                'file__rdf_type__curies': ['pcdmuse:PreservationMasterFile'],
+                'file__mime_type__str': 'image/tiff',
+            },
         ]},
     ]
     fields = iiif_links_fields(create_ctx(members))
